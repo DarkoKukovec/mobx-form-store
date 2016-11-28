412fb3d9b11adadf4ae8fbc57d44dc0ade980945
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2016-11-28 20:33:58 +0100

    feat(store): expose field store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /** @property Form store */
 export {FormStore} from './FormStore';
 
+/** @property Form field */
+export {FormFieldStore} from './FormFieldStore';
+
 /** @property Progress enum */
 export {progressEnum} from './utils/progressEnum';
