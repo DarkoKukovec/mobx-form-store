ef48e4e35745ec6aacd5d1cb4e55f8e125693080
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2016-11-28 20:32:06 +0100

    feat(store): add initial form and field store

--- src/FormFieldStore.js
+++ src/FormFieldStore.js
@@ -1,3 +1,18 @@
-export default class FormFieldStore {
+/** @module mobx-from-store/FormFieldStore */
 
+/**
+ * Class representing a Field for From store
+ *
+ * @class
+ * @exports Form field store
+ */
+export class FormFieldStore {
+
+  /**
+   * Creates an instance of FormFieldStore
+   *
+   * @constructor
+   */
+  constructor() {
+  }
 }
--- src/FormStore.js
+++ src/FormStore.js
@@ -1,10 +1,19 @@
-import FormFieldStore from './FormFieldStore';
+/** @module mobx-from-store/FormStore */
 
-class FormStore {
+/**
+ * Class representing a Form for a Mobx store
+ *
+ * @class
+ * @classdesc Form for a Mobx store
+ * @exports Form store
+ */
+export class FormStore {
 
+  /**
+   * Creates an instance of FormStore
+   *
+   * @constructor
+   */
+  constructor() {
+  }
 }
-
-module.exports = {
-  FormFieldStore,
-  FormStore
-};
